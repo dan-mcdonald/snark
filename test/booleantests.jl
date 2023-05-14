b2c65249d95cd7c9284fc10b722058dea44c6e0e
Daniel McDonald <daniel@mcd.onald.net>
2023-05-14 07:21:43 +0300

    add arith e2e test

--- test/booleantests.jl
+++ test/booleantests.jl
@@ -7,3 +7,7 @@
 @test arithmetize(:(!x[1] | !x[3])) == :((1-x[1]) + (1-x[3]) - (1-x[1]) * (1-x[3]))
 
 @test arithmetize(:((!x[1] & x[2]) & (x[3] | x[4]))) == :(((1 - x[1]) * x[2]) * ((x[3] + x[4]) - (x[3] * x[4])))
+
+import TypedPolynomials as TP
+@TP.polyvar x[1:4]
+@test snark.SumCheck(eval(arithmetize(:((!x[1] & x[2]) & (x[3] | x[4]))))) == 3
